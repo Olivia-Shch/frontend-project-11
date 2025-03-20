c503d37c73ce0e31056bf4d9db51f62ba2ecf338
vboxuser <vboxuser@Ubuntic.myguest.virtualbox.org>
2025-03-20 22:48:19 +1000

    eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+export default [
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      ".git/",
+      ".github/",
+      ".vercel/",
+      "package-lock.json",
+      "Makefile",
+      "README.md"
+    ],
+  },
+];
