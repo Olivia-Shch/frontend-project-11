7c7b922d05617212153d9258ac943bfc79923552
vboxuser <vboxuser@Ubuntic.myguest.virtualbox.org>
2025-03-21 15:07:33 +1000

    >_<

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
     ignores: [
-      "node_modules/",
-      "dist/",
-      ".git/",
-      ".github/",
-      ".vercel/",
-      "package-lock.json",
-      "Makefile",
-      "README.md"
+      'node_modules/',
+      'dist/',
+      '.git/',
+      '.github/',
+      '.vercel/',
+      'package-lock.json',
+      'Makefile',
+      'README.md',
     ],
   },
 ];
