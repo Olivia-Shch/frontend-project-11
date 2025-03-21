d2a084a97e07aba6704cfb194f1f9686f2a9a7e5
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-21 13:41:11 +1000

    Update and rename eslint.config.mjs to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      ".git/",
+      ".github/",
+      ".vercel/",
+      "package-lock.json",
+      "Makefile",
+      "README.md"
+    ],
+  },
+];
